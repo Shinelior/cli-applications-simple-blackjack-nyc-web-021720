838d789af432fce0e21a455bb6bbb1567aac0d47
lior Shine <shinelior@gmail.com>
2020-02-02 20:53:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -77,9 +77,9 @@ end
 def runner
 welcome
 card_total = initial_round
- hit?(card_total)
+ #hit?(card_total)
   # code runner here
-  display_card_total(card_total)
+  #display_card_total(card_total)
   until  card_total > 21
   card_total = hit?(card_total)
     display_card_total(card_total)
